e9dc8ffbb007fbbd3eab97c793474fe4cb307461
Joonas Salojärvi <joonas.salojarvi@cs.tamk.fi>
2018-09-25 18:54:30 +0300

    UI: Add placeholder Hello World window
    Temporary solution. UI class can be launched and it will open a windows. In the future, Main class will handle the opening of the software. This is just a placeholder.

--- UI.java
+++ UI.java
@@ -1,17 +1,29 @@
-
 import javafx.application.Application;
-import javafx.scene.*;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /* 
  *
  *
  */
-class UI extends Application{
+public class UI extends Application{
     
-    @Override
-    public void start(Stage stage){
+    @Override public void start(Stage stage) {
+        Text text = new Text(10, 40, "Hello World!");
+        text.setFont(new Font(40));
+        Scene scene = new Scene(new Group(text));
 
+        stage.setTitle("Welcome to JavaFX!"); 
+        stage.setScene(scene); 
+        stage.sizeToScene(); 
+        stage.show(); 
+    }
+
+    public static void main(String[] args){
+        Application.launch(args);
     }
     
 }
