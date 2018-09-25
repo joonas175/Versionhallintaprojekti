56085abecd0c427a8c0d9a91df443865f37e13b2
Toni Vänttinen <toni.vanttinen@cs.tamk.fi>
2018-09-25 18:40:14 +0300

    BmiLogic: Getter and Setter
    Add: Gets and Sets for height and weight

--- BmiLogic.java
+++ BmiLogic.java
@@ -1,9 +1,29 @@
 class BmiLogic {
 
-    private double size;
+    private double weight;
     private double height;
 
-    public BmiLogic(double newSize, double newHeight) {
+    public BmiLogic(double newWeight, double newHeight) {
         
+    }
+
+    public double getWeigth(){
+        return this.weight;
+    }
+
+    public double getHeight(){
+        return this.height;
+    }
+
+    public void setWeight(double newWeight){
+        if(newWeight > 0){
+            this.weight = newWeight;
+        }
+    }
+
+    public void setHeight(double newHeight){
+        if(newHeight > 0){
+            this.height = newWeigth;
+        }
     }
 }
