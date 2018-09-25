31805653da1396d9603decd2150a13f2ad775590
Lauri <laiho.lauri@luukku.com>
2018-09-25 18:33:00 +0300

    MilesConverterLogic: Add new attributes, getters, and setters.
    Add attributes miles and km. As well getters and setters for these attributes.

--- MilesConverterLogic.java
+++ MilesConverterLogic.java
@@ -1,3 +1,20 @@
 class MilesConverterLogic {
-    
+    private float miles;
+    private float km;
+
+    public void setMiles(float m) {
+        this.miles = m;
+    }
+
+    public float getMiles() {
+        return this.miles;
+    }
+
+    public void setKm(float k) {
+        this.km = k;
+    }
+
+    public float setKm() {
+        return this.km;
+    }
 }
