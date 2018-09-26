a3542d2ff19f26f8040d246704dcd7adef36b909
Joonas Salojärvi <joonas.salojarvi@cs.tamk.fi>
2018-09-26 21:57:43 +0300

    UI: Change text to "Pick a feature"
    Changed text to "Pick feature". This program will have many features in the future, and this will be the main window to launch them.

--- UI.java
+++ UI.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.awt.Button;
 
 /* 
  *
@@ -12,9 +13,13 @@ import javafx.stage.Stage;
 public class UI extends Application{
     
     @Override public void start(Stage stage) {
-        Text text = new Text(10, 40, "Hello World!");
+        Text text = new Text(10, 40, "Pick a feature");
         text.setFont(new Font(40));
-        Scene scene = new Scene(new Group(text));
+        Button bmiButton = new Button("BMI Calculator");
+        Group group = new Group();
+        group.getChildren().add(text);
+        Scene scene = new Scene(group);
+        
 
         stage.setTitle("Welcome to JavaFX!"); 
         stage.setScene(scene); 
