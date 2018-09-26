0d5fd77b0ba4be31c5b3f6b753b3b13310fb9c4a
Lauri <laiho.lauri@luukku.com>
2018-09-26 14:58:10 +0300

    Test: Testing miles to km
    Testing functionality to convert miles kilometers.

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 class Test {
     public static void main(String[] args) {
-        
+        MilesConverterLogic converter = new MilesConverterLogic();
+        System.out.println(converter.convertMilesToKm(60f) + "km");
     }
 }
