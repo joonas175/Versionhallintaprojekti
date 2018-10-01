1519634426f37b9409c668590b922af32df1c2b8
Toni Vänttinen <toni.vanttinen@cs.tamk.fi>
2018-10-01 19:39:57 +0300

    BmiUI: main and start methods
    Add: start method (empty)
    Add: main method that launches the screen

--- BmiUI.java
+++ BmiUI.java
@@ -1,3 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 class BmiUi {
-    
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+    public static void main(String[] args){
+        Application.launch(args);
+    }
 }
