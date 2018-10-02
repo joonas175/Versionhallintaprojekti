1edf84c7ce45a63c85626e39d949fa42c24b4e80
Joonas Salojärvi <joonas.salojarvi@cs.tamk.fi>
2018-10-02 10:42:28 +0300

    EurosToDollars: Add testing method
    Test the connection to a converter, with realtime course updates.

--- fi/tamk/testo/EurosToDollars.java
+++ fi/tamk/testo/EurosToDollars.java
@@ -1,7 +1,42 @@
 package fi.tamk.testo;
 
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.ProtocolException;
+
 public class EurosToDollars{
+
+    public static void main(String... args){
+        try{
+            test();
+        } catch (IOException e){
+            System.out.println(e);
+        } 
+        
+    }
+
     public static void convert(){
 
+    }
+
+    public static void test() throws IOException, ProtocolException{
+        URL url = new URL("http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setDoOutput(true);
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(inputLine);
     }
 }
