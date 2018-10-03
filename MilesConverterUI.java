a8f09785ec91d59d98128f84fe1ec7e5bf869ead
Lauri <laiho.lauri@luukku.com>
2018-10-03 16:55:18 +0300

    MilesConverterUI: Add Start method
    Add start method and javafx imports

--- MilesConverterUI.java
+++ MilesConverterUI.java
@@ -1,5 +1,12 @@
-class MilesToKmUI {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.stage.*;
+
+class MilesToKmUI extends Application{
+    public void start(Stage primaryStage) {
         
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
