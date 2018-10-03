7d5570d2af5f955291c01db852b4546b0f0cb8c0
Lauri <laiho.lauri@luukku.com>
2018-10-03 17:22:36 +0300

    MilesConverterUI: Add Constructor, move launch to Test
    Add Constructor, which launches the UI.

--- MilesConverterUI.java
+++ MilesConverterUI.java
@@ -1,18 +1,23 @@
+import javafx.scene.control.Button;
 import javafx.application.Application;
-import javafx.scene.*;
-import javafx.stage.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 
 class MilesToKmUI extends Application{
+
+    public MilesToKmUI(String[] args) {
+        Application.launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) {
-        Scene converterScene = new Scene(new Group());
+        GridPane pane = new GridPane();
+        Scene converterScene = new Scene(pane);
 
         primaryStage.setTitle("Miles to Kilometers converter.");
         primaryStage.setScene(converterScene);
         primaryStage.sizeToScene();
         primaryStage.show();
-    }
-    public static void main(String[] args) {
-        Application.launch(args);
     }
 }
