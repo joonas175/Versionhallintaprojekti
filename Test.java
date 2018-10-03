1d2b24713cef7de8ee04adb9ff0ef072ed8773bb
Lauri <laiho.lauri@luukku.com>
2018-10-03 17:21:16 +0300

    Test: Start the UI.
    Test now starts the MilesConverterUI.

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 class Test {
     public static void main(String[] args) {
         MilesConverterLogic converter = new MilesConverterLogic();
-        System.out.println(converter.convertMilesToKm(5.5f) + "km");
-        System.out.println(converter.convertKmToMiles(5.5f) + "mi");
+        MilesToKmUI application = new MilesToKmUI(args);
     }
 }
