f1769d41ffdcac01a73f1cab5d64e4c96314dc89
Lauri <laiho.lauri@luukku.com>
2018-10-03 17:04:39 +0300

    MilesConverterUI: Add javafx Scene
    Add Javafx scene that will hold the UI of the converter.

--- MilesConverterUI.java
+++ MilesConverterUI.java
@@ -3,8 +3,14 @@ import javafx.scene.*;
 import javafx.stage.*;
 
 class MilesToKmUI extends Application{
+    @Override
     public void start(Stage primaryStage) {
-        
+        Scene converterScene = new Scene(new Group());
+
+        primaryStage.setTitle("Miles to Kilometers converter.");
+        primaryStage.setScene(converterScene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
     }
     public static void main(String[] args) {
         Application.launch(args);
