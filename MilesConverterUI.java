a5d2022d51d96a106874f9a6bf88af62794dc276
Lauri <laiho.lauri@luukku.com>
2018-10-05 14:39:34 +0300

    MilesConverterUI: Add initPane method & fixes
    Add initPane method, which initializes pane components.
    Fix UI crash on startup, moved app launch to MilesConverterUI.

--- MilesConverterUI.java
+++ MilesConverterUI.java
@@ -3,21 +3,32 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
-class MilesToKmUI extends Application{
-
-    public MilesToKmUI(String[] args) {
-        Application.launch(args);
-    }
+public class MilesConverterUI extends Application{
 
     @Override
     public void start(Stage primaryStage) {
         GridPane pane = new GridPane();
+        initPane(pane);
         Scene converterScene = new Scene(pane);
 
         primaryStage.setTitle("Miles to Kilometers converter.");
         primaryStage.setScene(converterScene);
         primaryStage.sizeToScene();
         primaryStage.show();
+    }
+
+    public static void initPane(GridPane pane) {
+        Text text = new Text(10, 40, "Miles - KM converter");
+        text.setFont(new Font(40));
+        text.setTextAlignment(TextAlignment.CENTER);
+        Button convertButton = new Button("Convert!");
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test {
     public static void main(String[] args) {
-        MilesConverterLogic converter = new MilesConverterLogic();
-        MilesToKmUI application = new MilesToKmUI(args);
+        //MilesConverterLogic converter = new MilesConverterLogic();
+        //MilesConverterUI ui = new MilesConverterUI(args);
     }
 }
