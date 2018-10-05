84786cfbd6f8b6434723f71f68f3bdd36a1a7735
Toni Vänttinen <toni.vanttinen@cs.tamk.fi>
2018-10-05 13:43:28 +0300

    Main: add BmiLogic
    Add: creates BmiLogic object

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main(String... args){
-        BmiUI app = new BmiUI(args);
+        BmiLogic logic = new BmiLogic();
+        BmiUI app = new BmiUI(args, logic);
     }
 }
