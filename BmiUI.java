6ce6b8cb2fefc94b953734cca2be0ee33e91b155
Toni Vänttinen <toni.vanttinen@cs.tamk.fi>
2018-10-05 13:28:12 +0300

    BmiUI: BoxLayout
    Remove: BorderLayout
    Add: BoxLayout
    Add: result text area

--- BmiUI.java
+++ BmiUI.java
@@ -1,27 +1,30 @@
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.event.*;
-import java.awt.BorderLayout;
 
 class BmiUI extends JFrame implements ActionListener{
 
-    BorderLayout layout;
     JButton calculate;
     JButton exit;
     JTextArea heigthtxt;
     JTextArea weigthtxt;
+    JTextArea results;
 
     public BmiUI(String[] args) {
         calculate = new JButton("Calculate");
         exit = new JButton("Exit");
-        heigthtxt = new JTextArea(1,20);
-        layout = new BorderLayout();
-        setLayout(layout);
-        add(calculate,BorderLayout.PAGE_START);
-        add(heigthtxt,BorderLayout.CENTER);
-        add(exit,BorderLayout.PAGE_END);
+        heigthtxt = new JTextArea("Give heigth");
+        weigthtxt = new JTextArea("Give weigth");
+        results = new JTextArea("Results: ");
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        add(heigthtxt);
+        add(weigthtxt);
+        add(results);
+        add(calculate);
+        add(exit);
         calculate.addActionListener(this);
         exit.addActionListener(this);
         setTitle("BMI Calculator");
