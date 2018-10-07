eb78ec2b5d74a177270b1c02f4244ed4b775f9d3
Toni Vänttinen <toni.vanttinen@cs.tamk.fi>
2018-10-07 18:02:33 +0300

    BmiUI: Ui elements
    Add: Text for input windows and result
    Add: input windows
    Add: Buttons for calculate and exit

--- BmiUI.java
+++ BmiUI.java
@@ -5,6 +5,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class BmiUI extends Application {
 
@@ -20,7 +24,36 @@ public class BmiUI extends Application {
     }
 
     public static void makePane(GridPane pane) {
+        Text heighttxt = new Text("Height: ");
+        Text weighttxt = new Text("Weight: ");
+        Text result = new Text("Result: ");
+        TextArea heightInp = new TextArea();
+        TextArea weightInp = new TextArea();
+        Button calculateBtn = new Button("Calculate");
 
+        calculateBtn.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
+        });
+
+        Button exitBtn = new Button("Exit");
+
+        exitBtn.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
+        });
+
+        pane.add(heighttxt, 0,0,2,1);
+        pane.add(heightInp, 0,1,2,1);
+        pane.add(weighttxt, 0,2,2,1);
+        pane.add(weightInp, 0,3,2,1);
+        pane.add(result, 0,4,2,1);
+        pane.add(calculateBtn,0,5,1,1);
+        pane.add(exitBtn,1,5,1,1);
     }
 
     public static void main(String[] args) {
