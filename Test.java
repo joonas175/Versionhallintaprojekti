22de85f824eb616dc1b02c55d1783b921749f335
Lauri <laiho.lauri@luukku.com>
2018-10-08 18:26:06 +0300

    MilesConverter: Remove Test.java
    Removed Testing file Test.java

--- Test.java
+++
@@ -1,6 +0,0 @@
-class Test {
-    public static void main(String[] args) {
-        //MilesConverterLogic converter = new MilesConverterLogic();
-        //MilesConverterUI ui = new MilesConverterUI(args);
-    }
-}
